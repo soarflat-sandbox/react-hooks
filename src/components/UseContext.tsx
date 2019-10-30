88d64ff8c2921eb9c073a5d36dc9f5d98fe0fe58
soarflat <soarflat777@gmail.com>
2019-10-30 18:16:02 +0900

    fix: useContext のサンプルを修正 (#10)
    - 型を付与し忘れていたので型を付与した
    - コンテクストが更新されたら、そのコンテクストを利用しているコンポーネントが再描画されることを確認するためのロジックを追加した

--- src/components/UseContext.tsx
+++ src/components/UseContext.tsx
@@ -1,6 +1,15 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useState, useMemo } from 'react';
 
-const themes = {
+type ThemeProps = {
+  foreground: string;
+  background: string;
+};
+
+interface Themes {
+  [key: string]: ThemeProps;
+}
+
+const themes: Themes = {
   light: {
     foreground: '#000000',
     background: '#eeeeee'
@@ -13,25 +22,34 @@ const themes = {
 
 const ThemeContext = createContext(themes.light);
 
-const UseContext = () => {
+const UseContext: React.FC = () => {
+  const [theme, setTheme] = useState('dark');
+  const themeProps = useMemo<ThemeProps>(() => themes[theme], [theme]);
+  const handleToggleClick = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
   return (
     <>
-      <ThemeContext.Provider value={themes.dark}>
+      <ThemeContext.Provider value={themeProps}>
         <h2>UseContext</h2>
         <Component />
       </ThemeContext.Provider>
       <BrotherComponent />
+      <p>
+        <button onClick={handleToggleClick}>Toggle Theme</button>
+      </p>
     </>
   );
 };
 
-const Component = () => {
+const Component: React.FC = () => {
   return <GrandChildComponent />;
 };
 
-const GrandChildComponent = () => {
-  // ツリー内の最も近い上位の一致する`Provider`（`<ThemeContext.Provider value={themes.dark}>`）から
-  // コンテクストの値を取得する。そのため、このコンポーネントの`theme`は
+const GrandChildComponent: React.FC = () => {
+  // ツリー内の最も近い上位の一致する`Provider`（`<ThemeContext.Provider value={themeProps}>`）から
+  // コンテクストの値を取得する。そのため、このコンポーネントの初期描画時の`theme`は
   // {
   //   foreground: '#ffffff',
   //   background: '#222222'
@@ -46,7 +64,7 @@ const GrandChildComponent = () => {
   );
 };
 
-const BrotherComponent = () => {
+const BrotherComponent: React.FC = () => {
   // このコンポーネントはツリー内の最も近い上位の一致する`Provider`が存在しない（`<ThemeContext.Provider value={themes.dark}>`でラップされていない）。
   // そのため、コンテクスト生成時（`createContext(themes.light)`）に指定した初期値（`themes.light`）を利用する。
   // このコンポーネントの`theme`は
